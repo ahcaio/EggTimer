bf83c6b2d6de2d4499370a2bf6088245cecde781
Caio Cesar <ahcaio@hotmail.com>
2022-11-21 15:09:34 -0300

    - Add Progress bar - Add sound alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var player: AVAudioPlayer!
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        let totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if self.secondsPassed < totalTime{
+                
+                self.secondsPassed += 1
+                
+                let percentageProgress = Float(self.secondsPassed) / Float(totalTime)
+                self.progressBar.progress = percentageProgress
 
-
+                
+            } else {
+                self.timer.invalidate()
+                self.titleLabel.text = "Done!"
+                self.playSound()
+            }
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
